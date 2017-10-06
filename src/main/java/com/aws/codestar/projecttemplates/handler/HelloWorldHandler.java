4b407176893a3814b4120e87fbdbfaa34b19577c
Marcel Hanser <marcel.hanser@zuehlke.com>
2017-10-06 20:32:55 +0200

    Add git ignore and other stuff

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -6,16 +6,24 @@ import com.aws.codestar.projecttemplates.GatewayResponse;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 /**
  * Handler for requests to Lambda function.
  */
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 
+    private static String schmutz = null;
+
     public Object handleRequest(final Object input, final Context context) {
+
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         headers.put("Access-Control-Allow-Origin", "*");
-        return new GatewayResponse("{ \"Output\": \"Hello World!\"}", headers, 200);
+        if (schmutz == null) {
+            schmutz = UUID.randomUUID().toString();
+        }
+
+        return new GatewayResponse("{ \"Output\": \"Hello World! " + schmutz + "\"}", headers, 200);
     }
 }
