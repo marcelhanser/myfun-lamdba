806ca1c283aac9d73f01afe7895e3cb25da9f335
Jennifer Studer <jennifer.studer1@swisscom.com>
2017-12-26 09:14:01 +0100

    lambda handler implements  RequestHandler instead of RequestStreamHandler

--- src/main/java/com/aws/codestar/projecttemplates/handler/LambdaHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/LambdaHandler.java
@@ -5,41 +5,23 @@ import com.amazonaws.serverless.proxy.internal.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.aws.codestar.projecttemplates.Application;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-
-public class LambdaHandler implements RequestStreamHandler {
+public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
     private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
-    private static ObjectMapper mapper = new ObjectMapper();
-    private Logger log = LoggerFactory.getLogger(LambdaHandler.class);
 
     @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-            throws IOException {
+    public AwsProxyResponse handleRequest(AwsProxyRequest awsProxyRequest, Context context) {
         if (handler == null) {
             try {
                 handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+                return handler.proxy(awsProxyRequest, context);
 
             } catch (ContainerInitializationException e) {
                 e.printStackTrace();
-                outputStream.close();
-                return;
+                return null;
             }
-        }
-
-        AwsProxyRequest request = mapper.readValue(inputStream, AwsProxyRequest.class);
-
-        AwsProxyResponse resp = handler.proxy(request, context);
-        mapper.writeValue(outputStream, resp);
-        // just in case it wasn't closed by the mapper
-        outputStream.close();
+        } else return null;
     }
 }
